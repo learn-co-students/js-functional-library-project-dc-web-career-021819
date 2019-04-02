5d9e3273a1751f08a4847249d054ef7014ba6619
github username <github email address>
2019-04-02 15:59:25 -0400

    done

--- index.js
+++ index.js
@@ -100,9 +100,88 @@ fi = (function() {
         }
       }
       return newArray
+    },
+
+    sortBy: function(collection, callback){
+      newCollection = Array.isArray(collection) ? [...collection] : {...collection}
+      return newCollection.sort(function(a, b){
+        return callback(a) - callback(b)
+      })
+    },
+
+    uniq: function(collection, isSorted, callback){
+      itemsSoFar = []
+      if(typeof callback === "undefined"){
+        for(let i = 0; i < collection.length; i++){
+          if(itemsSoFar.includes(collection[i])){
+            collection.splice(i, 1);
+            i--;
+          } else{
+            itemsSoFar.push(collection[i]);
+          }
+        }
+      }
+      else {
+        for(let i = 0; i < collection.length; i++){
+          if(itemsSoFar.includes(callback(collection[i]))){
+            collection.splice(i, 1);
+            i--;
+          } else{
+            itemsSoFar.push(callback(collection[i]));
+          }
+        }
+      }
+      return collection
+    },
+
+    keys: function(object){
+      keyArray = []
+      for(let item in object){
+        keyArray.push(item)
+      }
+      return keyArray
+    },
+
+    values: function(object) {
+      keyArray = []
+      for(let item in object){
+        keyArray.push(object[item])
+      }
+      return keyArray
+    },
+
+    functions: function(object) {
+      functionNameArray = []
+      for(let item in object){
+        if(object[item] && object.toString.call(object[item]) === '[object Function]'){
+          functionNameArray.push(item)
+        }
+      }
+      return functionNameArray
+    },
+
+    flatten: function(array, shallow) {
+      if (shallow === true) {
+        newArray = []
+        for(let index in array){
+          console.log("type: "+ typeof array[index])
+          if(typeof array[index] === "object"){
+            for(let item in array[index]){
+              newArray.push(array[index][item])
+            }
+          }else{
+            newArray.push(array[index]);
+          }
+        }
+        console.log(newArray)
+        array = newArray;
+        return newArray;
+      } else {
+        array = array.toString()
+        array = array.replace("[", "").replace("]", "")
+        return array.split(",").map(item => parseInt(item))
+      }
     }
-
-
 
   }
 })()
