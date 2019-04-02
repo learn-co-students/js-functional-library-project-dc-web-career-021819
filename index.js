e259fe68060950c0e4f3b3f811e7ec36e1d2fc89
github username <github email address>
2019-04-02 11:12:49 -0400

    halfway there

--- index.js
+++ index.js
@@ -4,18 +4,47 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let i = 0;
+      for(let item in collection){
+        if(typeof item === "number"){
+          callback(item, i, collection);
+        } else {
+          callback(collection[item], i, collection);
+        }
+        i++;
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newCollection = [];
+      let i = 0;
+      for(let item in collection){
+        if(typeof item === "number"){
+          newCollection.push(callback(item, i, collection));
+        } else {
+          newCollection.push(callback(collection[item], i, collection));
+        }
+        i++;
+      }
+      return newCollection;
     },
 
     reduce: function() {
-
+      let newCollection = [];
+      let i = 0;
+      for(let item in collection){
+        if(typeof item === "number"){
+          newCollection.push(callback(item, i, collection));
+        } else {
+          newCollection.push(callback(collection[item], i, collection));
+        }
+        i++;
+      }
+      return newCollection;
     },
-    
+
     functions: function() {
 
     },
