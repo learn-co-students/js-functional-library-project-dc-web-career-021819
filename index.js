b428ea0861fc42664eaf41b924680f94f8433fe1
mmorales4050 <mmorales4050@gmail.com>
2019-04-02 13:52:24 -0400

    more funcs

--- index.js
+++ index.js
@@ -31,23 +31,77 @@ fi = (function() {
       return newCollection;
     },
 
-    reduce: function() {
-      let newCollection = [];
-      let i = 0;
-      for(let item in collection){
-        if(typeof item === "number"){
-          newCollection.push(callback(item, i, collection));
-        } else {
-          newCollection.push(callback(collection[item], i, collection));
-        }
-        i++;
+    reduce: function(collection, callback, acc)
+    {
+      let newCollection = [...collection]
+      if (!acc){
+        acc = newCollection.shift()
+        // var value = newCollection[0]
+      } //else{
+        var value = acc
+      //}
+      for(let item in newCollection){
+          value = callback(value, newCollection[item], newCollection)
       }
-      return newCollection;
+      return value
+    },
+    find: function(collection, predicate){
+      for(let item in collection) {
+        if(predicate(collection[item])){
+          return collection[item]
+        }
+      }
+    },
+    filter: function(collection, predicate){
+      let array = []
+      for(let item in collection) {
+        if(predicate(collection[item])){
+          array.push(collection[item])
+        }
+      }
+      return array
+    },
+    size: function(collection){
+      i = 0
+      for(let item in collection) {
+        i ++
+      }
+      return i
+    },
+    first: function(collection, num = 0){
+      let array = []
+      if (num !== 0){
+        for(let i = 0; i < num; i++) {
+            array.push(collection[i])
+        }
+      } else {
+        return collection[0]
+      }
+      return array
     },
 
-    functions: function() {
-
+    last: function(collection, num = 0){
+      let array = []
+      if (num !== 0){
+        for(let i = collection.length - 1; i > (collection.length - 1 - num); i--) {
+            array.unshift(collection[i])
+        }
+      } else {
+        return collection[collection.length - 1]
+      }
+      return array
     },
+
+    compact: function(collection){
+      let newArray = []
+      for(let item in collection) {
+        if(collection[item]){
+          newArray.push(collection[item])
+        }
+      }
+      return newArray
+    }
+
 
 
   }
